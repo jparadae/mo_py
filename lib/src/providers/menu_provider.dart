91933eacdb5866f213725fe185b98a536029d066
Javiera Parada Epul <javieraparadae@gmail.com>
2019-11-17 21:26:08 -0300

    Convirtiendo el json de String a un Map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 class _MenuProvider{
@@ -10,7 +12,12 @@ class _MenuProvider{
 
   _cargarJson(){//Metodo que carga la data
     rootBundle.loadString('data/menu_opts.json').then((data){//Del service.dart solo me importa el package boondle para exponer el json 
-      print(data); //exponiendo el path
+      //print(data); //exponiendo el path
+
+      //Transformando al String en un Mapa
+    Map dataJson = json.decode(data);
+    print(dataJson['rutas']);
+      opciones = dataJson['rutas'];
     });
   }
    
